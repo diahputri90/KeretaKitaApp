51b3a1dd907bdbbe6e2b890ebdde98f16d916893
diahputri90 <diahp90@gmail.com>
2020-12-29 05:46:34 +0800

    Initial commit

---
+++ app/src/main/java/com/diahl/keretakitaapp/activity/AboutUsActivity.java
@@ -0,0 +1,17 @@
+package com.diahl.keretakitaapp.activity;
+
+import android.os.Bundle;
+
+import com.diahl.keretakitaapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+    }
+}
